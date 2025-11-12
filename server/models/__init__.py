a18b4804b5addbad0682f1dea7a32cfb4332cfb4
Ndolo Mercy <mercymwendend444@gmail.com>
2025-11-12 09:26:33 +0300

    Initialize SQLAlchemy and Migrate in models; add empty routes module

---
+++ server/models/__init__.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+from .camper import Camper
+from .activity import Activity
+from .signup import Signup
